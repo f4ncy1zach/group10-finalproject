3d5ac93f05191ee0dc55cb793f47950cab84f2e3
Rain Zhang <tza103@sfu.ca>
2025-03-22 22:32:09 -0700

    Add project configuration and tailwind css set up

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/group10-finalproject/'
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
